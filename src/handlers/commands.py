3af6806a296cd4aaa96402af168a81f29825626e
LimeFox16 <16062006daniil@gmail.com>
2023-09-03 20:37:01 +0400

    updated logger message

--- src/handlers/commands.py
+++ src/handlers/commands.py
@@ -1,10 +1,11 @@
 from aiogram import types
 
-from src.misc.loader import dp, bot
+from src.misc.loader import dp, bot, logger
 
 
 @dp.message_handler(commands="start")
 async def start(message: types.Message):
+    logger.info("User starts the bot: %d", message.from_user.id)
     await bot.send_message(
         chat_id=message.from_user.id,
         text=f"<i>Попробуй вписать мой никнейм <code>{(await bot.get_me()).username}</code> в <b>любой чат</b> телеграмма</i>",
