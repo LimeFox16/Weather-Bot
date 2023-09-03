4a3622c66380c396a3cd85d18e85c73e30761c7f
LimeFox16 <16062006daniil@gmail.com>
2023-09-03 20:56:54 +0400

    some updates

--- src/handlers/commands.py
+++ src/handlers/commands.py
@@ -8,7 +8,7 @@ async def start(message: types.Message):
     logger.info("User starts the bot: %d", message.from_user.id)
     await bot.send_message(
         chat_id=message.from_user.id,
-        text=f"<i>Попробуй вписать мой никнейм <code>{(await bot.get_me()).username}</code> в <b>любой чат</b> телеграмма</i>",
+        text=f"Попробуй вписать мой никнейм <code>@{(await bot.get_me()).username}</code> в <b>любой чат</b> телеграмма",
         parse_mode="HTML"
     )
     
